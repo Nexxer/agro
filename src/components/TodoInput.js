a6e331900a5fb6f3f412c8c7223a04c8e995e1e4
Nexxer <yairopb@yandex.ru>
2021-11-15 18:22:00 +0300

    format code

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,33 +1,33 @@
 function InputForm({ handleChange, handleSubmit, item }) {
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <span class="input-group-text text-white bg-dark">
-              <i class="fa-lg fas fa-clipboard-list"></i>
-            </span>
-          </div>
-          <input
-            type="text"
-            className="form-control border border-primary"
-            placeholder="Введите название задачи"
-            aria-label="Введите название задачи"
-            aria-describedby="button-add"
-            onChange={handleChange}
-            value={item}
-            required
-          />
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span class="input-group-text text-white bg-dark">
+                            <i class="fa-lg fas fa-clipboard-list"></i>
+                        </span>
+                    </div>
+                    <input
+                        type="text"
+                        className="form-control border border-primary"
+                        placeholder="Введите название задачи"
+                        aria-label="Введите название задачи"
+                        aria-describedby="button-add"
+                        onChange={handleChange}
+                        value={item}
+                        required
+                    />
 
-          <div className="input-group-append">
-            <button className="btn btn-primary" type="submit" id="button-add">
-              Добавить
-            </button>
-          </div>
-        </div>
-      </div>
-    </form>
-  );
+                    <div className="input-group-append">
+                        <button className="btn btn-primary" type="submit" id="button-add">
+                            Добавить
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    );
 }
 
 export default InputForm;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,50 +1,50 @@
 function Todolist({ toDoLists, handleRemoveItem, handleStatus, handleClear }) {
-  const toDoSucces = toDoLists.reduce((total, item) => (item.done ? total + 1 : total), 0);
+    const toDoSucces = toDoLists.reduce((total, item) => (item.done ? total + 1 : total), 0);
 
-  function handleDeleteItem(id) {
-    handleRemoveItem(id);
-  }
+    function handleDeleteItem(id) {
+        handleRemoveItem(id);
+    }
 
-  function handleChangeStatus(id) {
-    handleStatus(id);
-  }
+    function handleChangeStatus(id) {
+        handleStatus(id);
+    }
 
-  return (
-    <>
-      <h2 className="text-center text-info">
-        Выполнено: <span className="">{toDoSucces + " из " + toDoLists.length}</span>
-      </h2>
-      <div className="card">
-        <ul className="list-group list-group-flush">
-          {toDoLists.map((toDoList, id) => {
-            return (
-              <li
-                className={`list-group-item d-flex justify-content-between align-items-center
+    return (
+        <>
+            <h2 className="text-center text-info">
+                Выполнено: <span className="">{toDoSucces + " из " + toDoLists.length}</span>
+            </h2>
+            <div className="card">
+                <ul className="list-group list-group-flush">
+                    {toDoLists.map((toDoList, id) => {
+                        return (
+                            <li
+                                className={`list-group-item d-flex justify-content-between align-items-center
                 ${toDoList.done ? "list-group-item-success" : ""}`}
-                key={id}
-              >
-                <p className="m-0" role="button" onClick={() => handleChangeStatus(toDoList.id)}>
-                  <i class="far fa-hand-point-right">&nbsp;</i>
-                  {toDoList.text}
-                </p>
-                <button
-                  type="button"
-                  class="close text-danger"
-                  aria-label="Close"
-                  onClick={() => handleDeleteItem(toDoList.id)}
-                >
-                  <i class="fas fa-trash"></i>
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <button className="btn btn-danger d-block ml-auto mr-auto mt-3" onClick={handleClear}>
-        Удалить все задания
-      </button>
-    </>
-  );
+                                key={id}
+                            >
+                                <p className="m-0" role="button" onClick={() => handleChangeStatus(toDoList.id)}>
+                                    <i class="far fa-hand-point-right">&nbsp;</i>
+                                    {toDoList.text}
+                                </p>
+                                <button
+                                    type="button"
+                                    class="close text-danger"
+                                    aria-label="Close"
+                                    onClick={() => handleDeleteItem(toDoList.id)}
+                                >
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <button className="btn btn-danger d-block ml-auto mr-auto mt-3" onClick={handleClear}>
+                Удалить все задания
+            </button>
+        </>
+    );
 }
 
 export default Todolist;
